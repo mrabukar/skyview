10348b0b6a31910109419fbe506512dfc3369be7
mrabukar <abukarmacalin08@gmail.com>
2026-07-30 15:51:00 +0300

    Add app/login/page.tsx

---
+++ web/app/login/page.tsx
@@ -0,0 +1,15 @@
+import { Suspense } from "react";
+import type { Metadata } from "next";
+import { LoginForm } from "./login-form";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+};
+
+export default function LoginPage() {
+  return (
+    <Suspense fallback={null}>
+      <LoginForm />
+    </Suspense>
+  );
+}
