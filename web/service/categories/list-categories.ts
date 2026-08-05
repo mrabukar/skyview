202e479171feae38501da581c670581210126291
mrabukar <abukarmacalin08@gmail.com>
2026-08-05 17:09:32 +0300

    Update web/service/categories/list-categories.ts

--- web/service/categories/list-categories.ts
+++
@@ -1,6 +0,0 @@
-import { apiFetch } from "@/service/client";
-import type { Category } from "@/types/categories/category";
-
-export function listCategories(): Promise<Category[]> {
-  return apiFetch<Category[]>("/api/categories");
-}
