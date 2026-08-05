9decb106346160c041324979796d52653d683ba2
mrabukar <abukarmacalin08@gmail.com>
2026-08-05 17:09:32 +0300

    Update web/service/categories/delete-category.ts

--- web/service/categories/delete-category.ts
+++
@@ -1,5 +0,0 @@
-import { apiFetch } from "@/service/client";
-
-export function deleteCategory(id: string): Promise<void> {
-  return apiFetch<void>(`/api/categories/${id}`, { method: "DELETE" });
-}
